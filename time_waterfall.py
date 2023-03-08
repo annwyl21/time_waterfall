dabf4c2191027213cf2f2cbb74439c1b35ed3766
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-08 15:36:32 +0000

    updated

--- time_waterfall.py
+++ time_waterfall.py
@@ -3,26 +3,29 @@ import random, time
 
 # prints random numbers between 1 and 9 across the terminal
 def time_waterfall():
-    for _ in range(200):
-        time.sleep(0.2)
+    for _ in range(175):
+        time.sleep(0.1)
         random_number = str(random.randint(1, 9))
-        print(random_number, end = '\r', flush=True) 
-        # \r replaces the previous random number with the new one
+        print(random_number, end = '', flush=True) 
 
-#time_waterfall()
+time_waterfall()
+print()
+print()
 
 # uses the iterable produced by range to countdown
 def countdown():
     for num in range(9, 0, -1):
-        time.sleep(1)
+        time.sleep(1.5)
         print(num, end = '\r', flush=True)
+        # \r replaces the previous random number with the new one
 
-#countdown()
+countdown()
+print()
 
 # marches across the screen and prints the numbers like cascading water
 def countdown_repeat():
     space = 0
-    for _ in range(50):
+    for _ in range(200):
         space +=1
         whitespace = space * ' '
         start = random.randint(1, 9)
